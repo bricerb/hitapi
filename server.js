4380a3ebd2033ac38b70068f7c021595b6360e76
rblanch-incomm <rblanch@incomm.com>
2018-06-29 08:23:34 -0400

    post

--- server.js
+++ server.js
@@ -12,6 +12,42 @@ app.get('/', function(req, res) {
 	res.send({'message': 'Back at ya!'});
 })
 
+app.post('/', function(req, res) {
+	var token = req.body.token;
+	var cardNumber = req.body.card;
+	var serial = req.body.serial;
+	var uri = 'https://staging.otcnetwork.com/WebApi/GetMemberDetails';
+	var opts = {
+		uri: uri,
+		method: 'post',
+		headers: {
+		  'Content-Type': 'application/json',
+		  'Authorization': 'Bearer ' + token
+		},
+		json:true,
+		body: {
+			"cardNumber": cardNumber, 
+		    "serialNumber": serial, 
+		    "apiVersion" : "2.0", 
+		    "mobileAppVersion": "1.0"
+		}
+	};
+
+	request(opts, function( err, resp, body ) {
+	if (err) {
+	  // response.body = err;
+	} else if (body != null) {
+		console.log(body)
+	  let balance = {'balance': body.cardBalance};
+	  res.send(balance);
+	  // response.continue();
+	} else {
+		res.send('NOPE');
+	    // response.error("No balance remaining.");
+	   }
+	});
+})
+
 app.listen(app.get('port'), () => {
-    console.log('Served on', app.get('port'));
+    console.log('Served onh', app.get('port'));
 });
